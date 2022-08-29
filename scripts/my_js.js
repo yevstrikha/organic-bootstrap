fc96385b512aca30c1a9405f306a7f53548671ca
yevstrikha <yevstrikha@gmail.om>
2022-08-28 22:16:56 -0400

    Our Customers

---
+++ scripts/my_js.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+})
